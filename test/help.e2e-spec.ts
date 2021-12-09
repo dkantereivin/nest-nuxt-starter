0f661e0e6f40d445f535f04b3b648e0f498d0bc3
David Kanter Eivin <david.kantereivin@gmail.com>
2021-12-09 15:37:09 -0500

    feat: add help e2e tests
    - tests may not pass until next few commits

--- test/help.e2e-spec.ts
+++ test/help.e2e-spec.ts
@@ -1,9 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import request from 'supertest';
+import { AppModule } from '@/core/app.module';
 
-describe('AppController (e2e)', () => {
+describe('Help (e2e)', () => {
     let app: INestApplication;
 
     beforeEach(async () => {
@@ -15,10 +15,11 @@ describe('AppController (e2e)', () => {
         await app.init();
     });
 
-    it('/ (GET)', () => {
+    it('ping -> pong', () => {
         return request(app.getHttpServer())
-            .get('/')
+            .post('/graphql')
+            .send({ query: '{help(name: "ping"){message}}' })
             .expect(200)
-            .expect('Hello World!');
+            .expect({ data: { help: { message: 'pong' } } });
     });
 });
