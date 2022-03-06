8a65c61d208969e5a674a654448ec3c63eb3cbc8
David Kanter Eivin <david.kantereivin@gmail.com>
2022-03-06 14:46:26 -0500

    test: add e2e tests for login mutation

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,19 @@
 import { Prisma, PrismaClient, User } from '@prisma/client';
 import faker  from '@faker-js/faker';
 import { hash } from 'bcryptjs';
+import {parse} from 'yaml';
+import {readFileSync} from 'fs';
+import _ = require('lodash');
 
 const prisma = new PrismaClient();
 
 const rawQueries = [];
+
+type StaticContent = {
+    users: Prisma.UserCreateInput[]
+}
+
+const staticContent: StaticContent = parse(readFileSync('prisma/seed.yaml', 'utf8'));
 
 export const seed = async () => {
     await prisma.$connect();
@@ -24,13 +33,20 @@ export const seedUsers = async (cnt: number): Promise<User[]> => {
         password: await hash(faker.internet.password(), 8)
     });
 
-    const users = await Promise.all(Array.from(
+    const randomUsers = await Promise.all(Array.from(
         {length: cnt},
         () => user()
     ));
 
+    const staticUsers = await Promise.all(
+        staticContent.users.map(async (user) => ({
+            ...user,
+            password: await hash(user.password, 8)
+        }))
+    );
+
     await prisma.user.createMany({
-        data: users
+        data: _.concat(randomUsers, staticUsers)
     });
 
     return prisma.user.findMany();
---
+++ test/core/auth.e2e-spec.ts
@@ -0,0 +1,59 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import request from 'supertest';
+import { AppModule } from '@/core/app.module';
+import { isJWT } from 'class-validator';
+
+describe('Auth (e2e)', () => {
+    let app: INestApplication;
+
+    beforeAll(async () => {
+        const moduleFixture: TestingModule = await Test.createTestingModule({
+            imports: [AppModule]
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
+
+    describe('MUT login()', () => {
+        const sendRequest = (usernameOrEmail: string, password: string) => {
+            return request(app.getHttpServer())
+                .post('/graphql')
+                .send({
+                    query: `mutation {login(usernameOrEmail: "${usernameOrEmail}", password: "${password}")}`
+                })
+                .expect(200);
+        };
+
+        it('should return a token for valid USERNAME + password', () => {
+            return sendRequest('example', 'example').expect((res) => {
+                expect(isJWT(res.body.data.login)).toBeTruthy();
+            });
+        });
+
+        it('should return a token for valid EMAIL + password', () => {
+            return sendRequest('example@example.com', 'example').expect(
+                (res) => {
+                    expect(isJWT(res.body.data.login)).toBeTruthy();
+                }
+            );
+        });
+
+        it('should fail for incorrect password', () => {
+            return sendRequest('example', 'WRONG_PASSWORD').expect({
+                errors: ['Invalid credentials provided.'],
+                data: null
+            });
+        });
+
+        it('should fail for inactive user', () => {
+            return sendRequest('inactive', 'inactive').expect({
+                errors: [
+                    'This user has been marked inactive. This can be corrected by an administrator.'
+                ],
+                data: null
+            });
+        });
+    });
+});
